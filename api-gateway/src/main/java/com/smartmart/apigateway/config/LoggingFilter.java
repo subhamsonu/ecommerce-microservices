76c3f31d7e4002cca50e4a0b28584dbe3265cb06
Subham Suar <subhamsonu076@gmail.com>
2026-04-20 20:38:50 +0530

    api-gateway file added
    api-gateway file added

---
+++ api-gateway/src/main/java/com/smartmart/apigateway/config/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.smartmart.apigateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class LoggingFilter implements GlobalFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("Incoming request: {} {}",
+                exchange.getRequest().getMethod(),
+                exchange.getRequest().getURI().getPath());
+
+        return chain.filter(exchange);
+    }
+}
